a4a09e14ab25cf7263a5e25639b8c08dcfb83a57
MykolaB <!q1w2e3r4>
2015-09-08 11:13:26 +0300

    added new examples

--- src/ch03/Tree.hs
+++ src/ch03/Tree.hs
@@ -1,3 +1,8 @@
-data Tree a = Node a (Maybe[Tree a]) (Maybe[Tree a])
-                deriving (Show)
+data Tree a = Node a (Tree a) (Tree a)
+            | Empty
+              deriving (Show)
+
+height Empty = 0
+height (Node x left right) = 1 + max(height left) (height right)
+
 
