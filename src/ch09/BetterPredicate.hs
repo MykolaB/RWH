bb5b7259f0735e829f921e8cce497ceb21d2d75c
MykolaB <!q1w2e3r4>
2015-09-18 18:50:45 +0300

    added new examples

--- src/ch09/BetterPredicate.hs
+++ src/ch09/BetterPredicate.hs
@@ -56,3 +56,34 @@ liftP q f k w x y z = f w x y z `q` k
 greaterP, lesserP :: (Ord a) => InfoP a -> a -> InfoP Bool
 greaterP = liftP (>)
 lesserP = liftP (<)
+
+simpleAndP :: InfoP Bool -> InfoP Bool -> InfoP Bool
+simpleAndP f g w x y z = f w x y z && g w x y z
+
+liftP2 :: (a -> b -> c) -> InfoP a -> InfoP b -> InfoP c
+liftP2 q f g w x y z = f w x y z `q` g w x y z
+
+andP = liftP2 (&&)
+orP = liftP2(||)
+
+liftPath :: (FilePath -> a) -> InfoP a
+liftPath f w _ _ _ = f w
+
+myTest2 = (liftPath takeExtension `equalP` ".hs") `andP`
+          (sizeP `greaterP` 131072)
+
+(==?) = equalP
+(&&?) = andP
+(>?) = greaterP
+
+myTest3 = (liftPath takeExtension ==? ".hs") &&? (sizeP >? 131072)
+
+infix 4 ==?
+infixr 3 &&?
+infix 4 >?
+
+myTest4 = liftPath takeExtension ==? ".hs" &&? sizeP >? 131072
+
+
+
+
